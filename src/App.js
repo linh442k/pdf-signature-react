c65f830481f51acaa15dbe41821ec4f19041523c
linh442k <nhatlinh4459@gmail.com>
2021-10-17 00:33:23 +0700

    Skeleton App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import { PDFDocument } from "pdf-lib";
+import "./App.css";
+import download from "downloadjs";
 
-function App() {
+export default function App() {
+  const [pdfFile, setPdfFile] = useState(null);
+  const [fileTitle, setFileTitle] = useState("");
+
+  // useEffect(() => {
+  //   fetch("https://pdf-lib.js.org/assets/with_update_sections.pdf").then(
+  //     (res) => console.log(res)
+  //   );
+  // }, []);
+
+  const fileReset = () => {
+    setPdfFile(null);
+    setFileTitle("");
+  };
+
+  const fileUpload = (e) => {
+    // console.log(e.target.files[0]);
+
+    let file = e.target.files[0];
+
+    setFileTitle(file.name);
+
+    let reader = new FileReader();
+
+    // setPdfFile(reader.readAsArrayBuffer(file));
+    reader.readAsArrayBuffer(file);
+
+    reader.onload = function () {
+      // console.log(reader.result);
+      setPdfFile(reader.result);
+    };
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sign" onClick={fileReset}>
+                Sign PDF
+              </Link>
+            </li>
+            <li>
+              <Link to="/check" onClick={fileReset}>
+                Check PDF Metadata
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Redirect exact from="/" to="/sign" />
+        {pdfFile !== null ? (
+          <div>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              {/* <Route exact path="/"> */}
+              {/* </Route> */}
+              <Route path="/sign">
+                <Sign pdfFile={pdfFile} title={fileTitle} />
+              </Route>
+              <Route path="/check">
+                <CheckMetadata pdfFile={pdfFile} />
+              </Route>
+            </Switch>
+          </div>
+        ) : (
+          <div>
+            <h2>Upload Here</h2>
+            <input
+              type="file"
+              id="pdf_picker"
+              accept=".pdf"
+              onChange={fileUpload}
+            />
+          </div>
+        )}
+      </div>
+      <div>File Name: {fileTitle}</div>
+    </Router>
+  );
+}
+
+function Sign({ pdfFile, title }) {
+  async function changeMetadata() {
+    const pdfDoc = await PDFDocument.load(pdfFile, {
+      ignoreEncryption: true,
+    });
+    pdfDoc.setSubject("");
+    // pdfDoc.setAuthor(pdfDoc.getAuthor());
+    // pdfDoc.setCreationDate(pdfDoc.getCreationDate());
+    // pdfDoc.setCreator(pdfDoc.getCreator());
+    // pdfDoc.setKeywords(pdfDoc.getKeywords);
+    // pdfDoc.setModificationDate(pdfDoc.getModificationDate());
+    // pdfDoc.setProducer(pdfDoc.getProducer());
+    // pdfDoc.setTitle(pdfDoc.getTitle());
+    // calculate signature
+    pdfDoc.setSubject("Hello This is Signature");
+    const pdfBytes = await pdfDoc.save();
+    download(pdfBytes, "(Signed)" + title, "application/pdf");
+  }
+
+  useEffect(() => {
+    console.log(pdfFile, title);
+  }, []);
+
+  return (
+    <div>
+      <h2>Sign PDF and Save File</h2>
+      <button onClick={changeMetadata}>Download Signed File</button>
     </div>
   );
 }
 
-export default App;
+function CheckMetadata({ pdfFile }) {
+  const [metadata, setMetadata] = useState("");
+
+  async function viewMetadata() {
+    const pdfDoc = await PDFDocument.load(pdfFile, {
+      updateMetadata: false,
+    });
+    // console.log(pdfDoc.getTitle());
+    setMetadata(pdfDoc.getSubject());
+  }
+
+  useEffect(() => {
+    console.log(pdfFile);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={viewMetadata}>Display Metadata</button>
+      <h2>Signature: {metadata}</h2>
+    </div>
+  );
+}
